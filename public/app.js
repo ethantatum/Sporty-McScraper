bd57bb4a67f536d14cdc17e7260b46e005f8778f
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-03-21 15:40:10 -0700

    updating to two buttons, better logic on viewing/adding comments

--- public/app.js
+++ public/app.js
@@ -7,7 +7,8 @@ $.get('/articles', function(data) {
         <h5 class='article-title'>${data[i].title}</h5><br>
         <h6 class='article-summary'>${data[i].summary}</h6><br>
         <a href='${data[i].link}' target='_blank'>${data[i].link}</a><br>
-        <button class='comment-button' data-id='${data[i]._id}'>Leave a Comment!</button></div><br>`);
+        <button class='comment-button' data-id='${data[i]._id}'>Leave a Comment!</button></div>
+        <button class='comment-button' data-id='${data[i]._id}'>View Comments!</button></div><br>`);
     }
 });
 
@@ -26,20 +27,23 @@ $(document).on('click', '.comment-button', function() {
         // Add comment information to page
         .then(function(data) {
             console.log(data.commentArr);
-            for (let j = 0; j < data.commentArr.length; j++) {
-                $('#comments').append(`<div class='comment-container'>
-                <h2 class='comment-title'>${data.title}</h2><br>
-                <input id='titleinput' name='title'><br>
-                <textarea id='bodyinput' name='body'></textarea><br>
-                <button data-id='${data._id}' id='savenote'>Save Comment</button></div>`);
-
-                if (data.commentArr) {
-                    $('#titleinput').val(data.commentArr[j].commentTitle);
-                    $('#bodyinput').val(data.commentArr[j].commentBody);
-                }
+            if (data.commentArr.length > 0) {
+                for (let j = 0; j < data.commentArr.length; j++) {
+                    $('#comments').append(`<div class='comment-container'>
+                    <input id='titleinput' name='title' value='${data.commentArr[j].commentTitle}'><br>
+                    <textarea id='bodyinput' name='body' placeholder='${data.commentArr[j].commentBody}'></textarea><br>
+                    <button data-id='${data._id}' id='savenote'>Save Comment</button></div>`);
+                    }
             }
+            else {
+                $('#comments').append(`<div class='comment-container'>
+                    <input id='titleinput' name='title' value='Insert Comment Title'><br>
+                    <textarea id='bodyinput' name='body' placeholder='Insert Comment Here'></textarea><br>
+                    <button data-id='${data._id}' id='savenote'>Save Comment</button></div>`);
+            }
+        
         });
-});
+    });
 
 // When user clicks save comment button
 $(document).on('click', '#savenote', function() {
