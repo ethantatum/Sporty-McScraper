06cfe2c13d72f1b7dd5f17da2d353cf94a0f7f7a
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-03-21 11:45:30 -0700

    completing app.js jquery

--- public/app.js
+++ public/app.js
@@ -6,19 +6,19 @@ $.get('/articles', function(data) {
         <p class='article-time'>${data[i].time}</p><br>
         <h5 class='article-title'>${data[i].title}</h5><br>
         <h6 class='article-summary'>${data[i].summary}</h6><br>
-        <a href='${this[i].link}' target='_blank'></a><br>
+        <a href='${data[i].link}' target='_blank'>${data[i].link}</a><br>
         <button class='comment-button' data-id='${data[i]._id}'>Leave a Comment!</button></div><br>`);
     }
 });
 
-// When user click comment button
-$('.comment-button').on('click', function() {
+// When user clicks comment button
+$(document).on('click', '.comment-button', function() {
     // Empty comments section
     $('#comments').empty();
     //Save ID
     let thisID = $(this).attr('data-id');
 
-    // AJAX call
+    // AJAX GET call
     $.ajax({
         method: 'GET',
         url: `/articles/${thisID}`
@@ -30,11 +30,33 @@ $('.comment-button').on('click', function() {
             <h2 class='comment-title'>${data.title}</h2><br>
             <input id='titleinput' name='title'><br>
             <textarea id='bodyinput' name='body'></textarea><br>
-            <button data-id='${data._id}' id='savenote'>Save Note</button></div>`);
+            <button data-id='${data._id}' id='savenote'>Save Comment</button></div>`);
 
             if (data.comment) {
                 $('#titleinput').val(data.comment.commentTitle);
                 $('#bodyinput').val(data.comment.commentBody);
             }
         });
+});
+
+// When user clicks save comment button
+$(document).on('click', '#savenote', function() {
+    // Grab ID
+    let thisID = $(this).attr('data-id');
+
+    // AJAX POST
+    $.ajax({
+        method: 'POST',
+        url: `/articles/${thisID}`,
+        data: {
+            commentTitle: $('#titleinput').val().trim(),
+            commentBody: $('#bodyinput').val().trim()
+        }
+    })
+    .then(function(data) {
+        console.log(data);
+        $('#comments').empty();
+    });
+    $('#titleinput').val('');
+    $('#bodyinput').val('');
 });
